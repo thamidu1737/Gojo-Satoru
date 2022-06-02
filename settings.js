d8be746eafd0f865851a749551184aff61fd67d9
thamidu1737 <106721408+thamidu1737@users.noreply.github.com>
2022-06-02 09:19:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94703044501']
+global.premium = ['94703044501']
+global.ownernomer = '94703044501'
+global.ownername = '🐦Thamiya'
+global.botname = 'Thamiya'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
